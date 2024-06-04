d88a02a9dfbc79b5e8ab26aac66e17fdc8afc7d9
Carlos Hernández <153103550+Carloshrndz@users.noreply.github.com>
2024-06-04 17:46:26 -0600

    Add files via upload

--- doc/InstallDependencies.bat
+++ doc/InstallDependencies.bat
@@ -1,3 +1,8 @@
-pip install pyttsx3
+pip install pyttsx3 
+pip install customtkinter
+pip install CTkMessagebox
+pip install googletrans
+pip install openai
+pip install --upgrade openai httpx
 pip install spacy
 py -m spacy download en_core_web_sm
